674218e5870e42edaa758ccb6cd3359cd848ed90
Kevin Johnson <kevin@coolkev.com>
2018-03-06 00:29:47 -0500

    Default to current domain if hostname isn't specified

--- viewer/src/connection/constants/ConnectionConstants.js
+++ viewer/src/connection/constants/ConnectionConstants.js
@@ -2,6 +2,6 @@ import * as QueryParams from "../../queryParams/QueryParams";
 
 const params = QueryParams.get();
 
-export const HOST = params["host"] ? params["host"] : "iframe.coolkev.com";
+export const HOST = params["host"] ? params["host"] : window.location.hostname;
 export const PORT = parseInt(params["port"], 10) ? parseInt(params["port"], 10) : 10050;
 export const DEFAULT_ROOM = params["room"] ? params["room"] : "default";

52ccd3e8119f27b8e0d2d7f3c143be9616da51b8
Kevin Johnson <kevin@coolkev.com>
2018-03-20 03:33:25 -0400

    Include scaling logic for iframes in the viewer. Included the youtube iframe api js codeY

--- viewer/src/iframes/IFrames.js
+++ viewer/src/iframes/IFrames.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import queryString from 'query-string';
 import './IFrames.css';
 
 const DEFAULT_PROFILE = "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg?sz=50";
@@ -9,15 +10,36 @@ function Avatar(props) {
   return (
     <div className="avatar">
       {!isDefaultProfile && <img alt="profile" className="profile" src={props.subject.picture}/>}
-      {isDefaultProfile  && <span className="name">{props.subject.name}</span>}
+      {isDefaultProfile && <span className="name">{props.subject.name}</span>}
     </div>
   )
+}
+
+function getIFrameStyle(config) {
+  const qs = config.link.substring(config.link.indexOf('?') + 1);
+  const iframeParams = queryString.parse(qs);
+  const style = {};
+  if (iframeParams.IFRAME_SCALE) {
+    const scale = parseFloat(iframeParams.IFRAME_SCALE);
+    style["transform"] = `scale(${iframeParams.IFRAME_SCALE})`;
+    style["transformOrigin"] = `top left`;
+    style["width"] = `${100/scale}%`;
+    style["height"] = `${100/scale}%`;
+  }
+  return style;
 }
 
 function IFrame(props) {
   return (
     <div className={props.className + (props.config.isFeatured ? " featured" : "")}>
-      <iframe title={props.className.replace(/ /g, "")} src={props.config.link ? props.config.link : "http://iframe.coolkev.com/start.html"} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen/>
+      <iframe
+        title={props.className.replace(/ /g, "")}
+        src={props.config.link ? props.config.link : "http://iframe.coolkev.com/start.html"}
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        style={getIFrameStyle(props.config)}
+      />
       <Avatar subject={props.config.who}/>
     </div>
   )
